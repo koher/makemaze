7ab4fcffbf238bab3e3eaba768ba5810738aa5d3
Yuta Koshizawa <koher@koherent.org>
2019-12-27 06:47:47 +0900

    Separate code to make mazes to MazeMaking.swift

--- Sources/makemaze/main.swift
+++ Sources/makemaze/main.swift
@@ -4,43 +4,6 @@ import SwiftImage
 let defaultWidth = 1023
 let defaultHeight = 1023
 
-enum Cell: Equatable {
-    case start
-    case goal
-    case path
-    case wall(Int)
-    
-    var color: RGBA<UInt8> {
-        switch self {
-        case .start:   return .init(0x0000FFFF)
-        case .goal:    return .init(0xFF0000FF)
-        case .path:    return .init(0xFFFFFFFF)
-        case .wall(_): return .init(0x000000FF)
-        }
-    }
-}
-
-func +(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
-    return (lhs.0 + rhs.0, lhs.1 + rhs.1)
-}
-
-func +=(lhs: inout (Int, Int), rhs: (Int, Int)) {
-    lhs = lhs + rhs
-}
-
-func *(lhs: (Int, Int), rhs: Int) -> (Int, Int) {
-    return (lhs.0 * rhs, lhs.1 * rhs)
-}
-
-let directions: [(Int, Int)] = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-
-extension Image {
-    subscript(point: (Int, Int)) -> Pixel {
-        get { self[point.0, point.1] }
-        set { self[point.0, point.1] = newValue }
-    }
-}
-
 // Input
 let width: Int
 let height: Int
@@ -67,81 +30,8 @@ do {
     }
 }
 
-// Making a maze using the algorithm
-// explained at https://algoful.com/Archive/Algorithm/MazeExtend
-var maze: Image<Cell> = .init(width: width, height: height) { x, y in
-    if x == 0 || y == 0 || x == width - 1 || y == height - 1 {
-        return .wall(0)
-    } else {
-        return .path
-    }
-}
-
-maze[1, 1] = .start
-if Bool.random() {
-    maze[2, 1] = .wall(0)
-    maze[2, 2] = .wall(0)
-} else {
-    maze[1, 2] = .wall(0)
-    maze[2, 2] = .wall(0)
-}
-
-maze[width - 2, height - 2] = .goal
-if Bool.random() {
-    maze[width - 3, height - 2] = .wall(0)
-    maze[width - 3, height - 3] = .wall(0)
-} else {
-    maze[width - 2, height - 3] = .wall(0)
-    maze[width - 3, height - 3] = .wall(0)
-}
-
-var pointsToStartWall: [(Int, Int)] = []
-for y in maze.yRange {
-    guard y.isMultiple(of: 2) else { continue }
-    for x in maze.xRange {
-        guard x.isMultiple(of: 2) else { continue }
-        pointsToStartWall.append((x, y))
-    }
-}
-pointsToStartWall.shuffle()
-
-var wallCount: Int = 0
-wallsMaking: while let (x, y) = pointsToStartWall.popLast() {
-    guard maze[x, y] == .path else { continue }
-    
-    wallCount += 1
-    
-    var wallPoints: [(Int, Int)] = []
-    
-    var point = (x, y)
-    wallMaking: while true {
-        maze[point] = .wall(wallCount)
-        wallPoints.append(point)
-
-        for direction in directions.shuffled() {
-            guard maze[point + direction] == .path else { continue }
-            if maze[point + direction * 2] == .wall(wallCount) { continue }
-            
-            point += direction
-            maze[point] = .wall(wallCount)
-            
-            point += direction
-            if maze[point] == .path {
-                continue wallMaking
-            } else {
-                continue wallsMaking
-            }
-        }
-        
-        guard let _ = wallPoints.popLast() else {
-            preconditionFailure("Never reaches here.")
-        }
-        guard let nextPoint = wallPoints.popLast() else {
-            preconditionFailure("Never reaches here.")
-        }
-        point = nextPoint
-    }
-}
+// Making a maze
+let maze: Image<Cell> = .init(width: width, height: height)
 
 // Output
 let data: Data = maze.map { $0.color }.data(using: .png)!
